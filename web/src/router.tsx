8e4de21d0f3e7acc92a3be6e2ae038f05d3f14aa
wxjing46@163.com <wxjing46@163.com>
2022-09-08 22:23:43 +0800

    路由

---
+++ web/src/router.tsx
@@ -0,0 +1,20 @@
+import React, { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../page/Home';
+const Find = lazy(() => import('../page/Find'));
+const RouteConfigs = () => {
+    return (
+        <div>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    {/* <Route path="/" element={<Home />}></Route>
+                    <Route path='/:id' element={<Homeimg />}></Route>
+                    <Route path="/find" element={<Find />}></Route>
+                    <Route path="/vip" element={<Vip />}></Route>
+                    <Route path="/mine" element={<Mine />}></Route> */}
+                </Routes>
+            </Suspense>
+        </div>
+    );
+};
+export default RouteConfigs;
