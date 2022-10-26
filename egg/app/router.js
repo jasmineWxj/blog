26f6eeaff6384b3c5c43013bda7bd7e2e1dd2dda
wxjing46@163.com <wxjing46@163.com>
2022-10-26 22:19:21 +0800

    feat: post

--- egg/app/router.js
+++ egg/app/router.js
@@ -11,7 +11,7 @@ module.exports = app => {
   router.get('/getmessage', controller.home.getmessage);
   router.get('/delmessage', controller.home.delmessage);
   router.get('/getmd', controller.home.getmd);
-  router.get('/postmd', controller.home.postmd);
+  router.post('/postmd', controller.home.postmd);
 
   router.post('/postimg', controller.home.postimg);
 };
